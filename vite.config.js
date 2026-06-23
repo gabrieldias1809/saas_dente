2c59e6644c24453fa7bb8ff5fcd8e969d301c7f7
Gabriel Almeida Dias <diasbusiness18@gmail.com>
2026-06-23 19:44:11 -0300

    build(frontend): add vite.config.js to include laudo.html in the build output

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        laudo: resolve(__dirname, 'laudo.html')
+      }
+    }
+  }
+});
